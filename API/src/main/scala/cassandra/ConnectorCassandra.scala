ce2d12a807e7f7012df044e06585ac55744d9fee
LAGINHA Guillaume <darkanos00@gmail.com>
2022-11-07 22:30:41 +0100

    fix connector

--- API/src/main/scala/cassandra/ConnectorCassandra.scala
+++ API/src/main/scala/cassandra/ConnectorCassandra.scala
@@ -1,5 +1,24 @@
 package cassandra
 
-class ConnectorCassandra {
+import com.datastax.oss.driver.api.core.{CqlIdentifier, CqlSession}
+import com.datastax.oss.driver.api.querybuilder.SchemaBuilder
 
+import java.net.InetSocketAddress
+
+object ConnectorCassandra {
+  def connect(): CqlSession = {
+    val builder = CqlSession.builder()
+    builder.addContactPoint(new InetSocketAddress("localhost",9042))
+    builder.withLocalDatacenter("datacenter")
+    val session = builder.build()
+    return session
+  }
+
+
+
+  def main(args : Array[String]) : Unit =
+    val session =  connect();
+
+
+    session.close()
 }
